aab13936c0cdbd31a64cc59a14af8e16aa94b4d4
Vivienhrm <vivienherrmann67350@gmail.com>
2025-10-20 15:45:20 +0200

    gestion des quantitées d'outils du panier bd

--- sql/reserv/01-charlyreserv.schema.sql
+++ sql/reserv/01-charlyreserv.schema.sql
@@ -7,7 +7,15 @@ CREATE TABLE public.reservations (
     date_creation TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     statut VARCHAR(50) NOT NULL DEFAULT 'En attente',
     prix_total NUMERIC(10, 2) NOT NULL CHECK (prix_total >= 0),
-    
+
     -- Contrainte pour s'assurer que la date de début est avant la date de fin
     CONSTRAINT chk_date_range CHECK (date_debut <= date_fin)
+);
+
+DROP TABLE IF EXISTS public.reservation_outils CASCADE;
+CREATE TABLE public.reservation_outils (
+    id_reservation uuid REFERENCES reservations(id_reservation) ON DELETE CASCADE,
+    id_outil INTEGER REFERENCES outils(id_outil) ON DELETE CASCADE,
+    quantite INTEGER NOT NULL DEFAULT 1 CHECK (quantite > 0),
+    PRIMARY KEY (id_reservation, id_outil)
 );
