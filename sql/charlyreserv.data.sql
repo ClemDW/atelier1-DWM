9f6347ee649c931521e35fb907a97f83804d527d
ClemDW <clementdewasch54@gmail.com>
2025-10-20 15:01:45 +0200

    fix: sql data

--- sql/charlyreserv.data.sql
+++ sql/charlyreserv.data.sql
@@ -1,3 +1,3 @@
 INSERT INTO public.reservations (id_reservation, id_utilisateur, date_debut, date_fin, statut, prix_total) VALUES
-(gen_random_uuid(), 'fbdb2514-4caf-4864-afca-89b565f5eece', '2025-10-01', '2025-10-05', 'Validé', 175.00),
-(gen_random_uuid(), 'fbdb2514-4caf-4864-afca-89b565f5eece', '2025-10-02', '2025-10-02', 'En Attente', 0.00)
+('a41ccf5c-9365-4232-a6e7-9b62740b60e6', 'fbdb2514-4caf-4864-afca-89b565f5eece', '2025-10-01', '2025-10-05', 'Validé', 175.00),
+('f30cdbf8-0412-4dd5-a439-b8c173bf11cc', 'fbdb2514-4caf-4864-afca-89b565f5eece', '2025-10-02', '2025-10-02', 'En Attente', 0.00)
